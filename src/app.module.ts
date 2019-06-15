c7015c505d6d91b549df03734da1d78006b0303c
emirluna <luis.emir.lm@gmail.com>
2019-06-15 00:05:05 -0500

    Crud de una Tabla en MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import {MongooseModule} from '@nestjs/mongoose'
 
 @Module({
-  imports: [],
+  imports: [
+    ProductModule, 
+    MongooseModule.forRoot('mongodb://localhost/products',{
+      useNewUrlParser: true
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
